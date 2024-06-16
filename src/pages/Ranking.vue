ef14af8bc62f52a13b68095b05fa4c85e9310d51
anko <daiusa3@icloud.com>
2024-06-16 11:32:29 +0900

    merge

--- src/pages/Ranking.vue
+++ src/pages/Ranking.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import Table from '@/components/Table.vue'
+import { components } from '@/openapi.gen';
+import createClient from "openapi-fetch";
 import { ref } from 'vue';
 
 const items = ref([
@@ -8,6 +10,10 @@ const items = ref([
   { rank: 3, iconUri: "https://q.trap.jp/api/v3/public/icon/anko", userName: "あんこ", score: 2041, level: 1, timeStamp: '' },
   { rank: 4, iconUri: "https://q.trap.jp/api/v3/public/icon/anko", userName: "あんこ", score: 2041, level: 1, timeStamp: '' }
 ])
+
+const client = createClient<components['schemas']['']>({ baseUrl: "https://myapi.dev/v1/" });
+
+https://kusa.trap.show/api/rankings?count=10&level=1
 </script>
 
 <template>
