7b2592bd8a4b3fa5ec7e4f92bc6ed255d88c72df
anko <daiusa3@icloud.com>
2024-06-16 14:01:55 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/game/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
