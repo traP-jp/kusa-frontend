b2c9934f4ac1b07412d6c098de99d932c48989bc
superharuma <mokontaharu@ciloud.com>
2024-06-16 13:05:58 +0900

    merge

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,25 @@
-import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
+import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/pages/App.vue'),
+    path: "/",
+    name: "Home",
+    component: () => import("@/pages/App.vue"),
   },
   {
-    path: '/ranking',
-    name: 'Ranking',
-    component: () => import('@/pages/Ranking.vue'),
+    path: "/ranking",
+    name: "Ranking",
+    component: () => import("@/pages/Ranking.vue"),
   },
   {
-    path: '/play',
-    name: 'Play',
-    component: () => import('@/pages/PlayScreen.vue'),
+    path: "/play",
+    name: "Play",
+    component: () => import("@/pages/PlayScreen.vue"),
+  },
+  {
+    path: "/result",
+    name: "Result",
+    component: () => import("@/pages/Result.vue"),
   },
   // {
   //   path: '/rooms/:id',
@@ -32,11 +37,11 @@ const routes: RouteRecordRaw[] = [
   //   name: 'NotFound',
   //   component: () => import('@/pages/NotFound.vue'),
   // }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
