90439264561692057882ff6d0e45846f333f0666
anko <daiusa3@icloud.com>
2024-06-16 14:05:07 +0900

    🎨 Test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "https://kusa.trap.show/game/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
